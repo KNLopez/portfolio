2484ec18793950f574b91d7704123658920fd886
kurtdevph <kurtdevph@gmail.com>
2024-05-21 15:52:29 +0800

    improve layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -32,39 +32,59 @@ export default function Home() {
       <main className="flex min-h-screen flex-col justify-between lg:p-36 p-8 container gap-8 lg:flex-row lg:gap-36">
         <div className="background-container fixed inset-0 z-10" />
         <div className="flex flex-1 flex-col gap-2 w-full z-20 relative">
-          <div className="sticky top-36 lg:bg-[rgb(25,25,25)] lg:p-20 rounded">
+          <div className="sticky top-36 lg:p-6 rounded">
             <h1 className="text-6xl font-bold mb-2">Kurt Lopez</h1>
             <p className="text-lg mb-4 uppercase font-bold tracking-wider ">
               Senior Frontend Engineer
             </p>
-            <p className="text-gray-400 text-sm mb-2">
+            <p className="text-gray-300 text-sm mb-2">
               I&apos;m a Senior Frontend Engineer from the Philippines with over
               9 years of experience in building beautiful UI. I specialize in
               React, Next.js, and Typescript.
             </p>
-            <p className="text-gray-400 text-sm">
+            <p className="text-gray-300 text-sm">
               When I&apos;m not coding, I enjoy playing video games, watching
               movies, and traveling.
             </p>
-            <div className="mt-4 text-sm">
+            <div className="mt-4 text-sm grid lg:grid-cols-2 gap-4 tracking-wide">
               <span>
-                email:
+                Email:
                 <a
                   target="_blank"
                   href="mailto:kurtdevph@gmail.com"
-                  className="text-zinc-400 hover:text-white underline ml-1"
+                  className="text-zinc-200 hover:text-white underline ml-1 font-semibold"
                 >
                   kurtdevph@gmail.com
                 </a>
               </span>
-              <span className="ml-4">
-                phone:
+              <span>
+                Phone:
                 <a
                   target="_blank"
                   href="tel:+6591733474"
-                  className="text-zinc-400 hover:text-white underline ml-1"
+                  className="text-zinc-200 hover:text-white underline ml-1 font-semibold"
                 >
                   +6591733474
+                </a>
+              </span>
+              <span>
+                Behance:
+                <a
+                  target="_blank"
+                  href="https://www.behance.net/kurtdevph"
+                  className="text-zinc-200 hover:text-white underline ml-1 font-semibold"
+                >
+                  behance.net/kurtdevph
+                </a>
+              </span>
+              <span>
+                Codepen:
+                <a
+                  target="_blank"
+                  href="https://codepen.io/kurtlopez"
+                  className="text-zinc-200 hover:text-white underline ml-1 font-semibold"
+                >
+                  codepen.io/kurtlopez
                 </a>
               </span>
             </div>
